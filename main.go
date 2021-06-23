7b1a69f96132911b5acf24f7c5adebd844e46428
tatatia <thetattianochka@gmail.com>
2021-06-23 20:19:07 +0300

    opened the dynamic sides

--- main.go
+++ main.go
@@ -1,10 +1,21 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
 	"html/template"
 	"net/http"
 )
+
+type Article struct {
+	Id                     uint16
+	Title, Anons, Fulltext string
+}
+
+var posts = []Article{}
+var showPost = Article{}
 
 func index(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("templates/index.html", "templates/header.html", "templates/footer.html")
@@ -13,7 +24,32 @@ func index(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, err.Error())
 	}
 
-	t.ExecuteTemplate(w, "index", nil)
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/mydb")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	//виборка даних
+	res, err := db.Query("SELECT * FROM `articles`")
+	if err != nil {
+		panic(err)
+	}
+
+	posts = []Article{}
+	for res.Next() {
+		var post Article
+		err = res.Scan(&post.Id, &post.Title, &post.Anons, &post.Fulltext)
+		if err != nil {
+			panic(err)
+		}
+
+		posts = append(posts, post)
+
+		//fmt.Println((fmt.Sprintf("Post: %s with id: %d", post.Title, post.Id)))
+
+	}
+
+	t.ExecuteTemplate(w, "index", posts)
 }
 
 func create(w http.ResponseWriter, r *http.Request) {
@@ -31,13 +67,71 @@ func save_article(w http.ResponseWriter, r *http.Request) {
 	anons := r.FormValue("anons")
 	full_text := r.FormValue("full_text")
 
+	if title == "" || anons == "" || full_text == "" {
+		fmt.Fprintf(w, "Не всі заповнені дані")
+	} else {
+		db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/mydb")
+		if err != nil {
+			panic(err)
+		}
+		defer db.Close()
+
+		//установка даних
+		insert, err := db.Query(fmt.Sprintf("INSERT INTO `articles` (`title`,"+
+			" `anons`,`full_text`) VALUES ('%s', '%s','%s')", title, anons, full_text))
+		if err != nil {
+			panic(err)
+		}
+		defer insert.Close()
+
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	}
+}
+
+func show_post(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	t, err := template.ParseFiles("templates/show.html", "templates/header.html", "templates/footer.html")
+
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/mydb")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	res, err := db.Query(fmt.Sprintf("SELECT * FROM `articles` WHERE `id` ='%s'", vars["id"]))
+	if err != nil {
+		panic(err)
+	}
+
+	showPost = Article{}
+	for res.Next() {
+		var post Article
+		err = res.Scan(&post.Id, &post.Title, &post.Anons, &post.Fulltext)
+		if err != nil {
+			panic(err)
+		}
+
+		showPost = post
+
+	}
+
+	t.ExecuteTemplate(w, "show", showPost)
 }
 
 func handleFunc() {
+	rtr := mux.NewRouter()
+	rtr.HandleFunc("/", index).Methods("GET")
+	rtr.HandleFunc("/create", create).Methods("GET")
+	rtr.HandleFunc("/save_article", save_article).Methods("POST")
+	rtr.HandleFunc("/post/{id:[0-9]+}", show_post).Methods("GET")
+
+	http.Handle("/", rtr)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
-	http.HandleFunc("/", index)
-	http.HandleFunc("/create", create)
-	http.HandleFunc("/save_article", save_article)
 	http.ListenAndServe(":8080", nil)
 }
 
